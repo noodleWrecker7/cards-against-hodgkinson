c95f90cf4e04ea84bf255ea711adca5f1f86f1ab
hodgk <hodgkinson.adam@gmail.com>
2021-02-16 22:18:02 +0000

    CI/CD for Google cloud and Netlify should be good to go

--- server/server.js
+++ server/server.js
@@ -1,2 +1,32 @@
 console.log('Server Starting')
 console.log('Current Build: ' + process.env.GAE_VERSION)
+
+// todo express, socket.io, sysinfo
+
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:8080',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
+    allowEIO3: true
+  }
+})
+const PORT = process.env.PORT || 6999
+
+io.on('connection', function (socket) {
+  // handle sockets
+
+}
+)
+
+app.get('/*', function (request, response) {
+  console.log(request.path)
+  response.send('<html><script>window.location.href="cards.adamhodgkinson.dev"</script></html>')
+})
+
+http.listen(PORT, () => {
+  console.log('Listening on:' + PORT)
+})
--- src/components/whitecard.vue
+++ src/components/whitecard.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-  name: 'Game'
+  name: 'whitecard'
 }
 </script>
 
